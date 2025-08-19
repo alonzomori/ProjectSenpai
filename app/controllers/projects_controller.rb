7d813dfdcbbcfabeb52940c1c148f32bf75bf340
richard williams <richard.williams5697@yahoo.com>
2025-08-19 18:10:50 +0900

    index finished

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,10 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def show
+    @project = @projects.find(params[:id])
+  end
+
   def new
     @project = Project.new
   end

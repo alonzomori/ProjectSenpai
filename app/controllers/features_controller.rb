f13203e200b49ff14067420a1e1d428343a5889a
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-20 17:09:52 +0900

    Made Changes To Edit View for Features

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -4,7 +4,26 @@ class FeaturesController < ApplicationController
     @message = Message.new
   end
 
-  def create
+  def index
+    @features = Feature.all
   end
-  
+
+  def edit
+    @feature = Feature.find(params[:id])
+  end
+
+  def update
+    @feature = Feature.find(params[:id])
+    if @feature.update(feature_params)
+      redirect_to @feature, notice: "Sorry, this feature cannot be displayed."
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def feature_params
+    params.require(:feature).permit(:name, :date_finished, :summary)
+  end
 end

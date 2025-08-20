bdaeab97689da17e67fefaa64d57472a24e5d3ca
richard williams <richard.williams5697@yahoo.com>
2025-08-20 15:11:02 +0900

    project create working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,16 +4,22 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    @project_new = Project.new
+    @project = Project.new
   end
 
   def create
     @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_features_path(@project), notice: "Project was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def project_params
-    params.require(:projects).permit(:name, :description)
+    params.require(:project).permit(:name, :description)
   end
 end

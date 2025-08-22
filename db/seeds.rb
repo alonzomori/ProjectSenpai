159b96691786bbff8f4774771c82716d4f7cc923
Andrew Leemhuis <tanukilandslide@gmail.com>
2025-08-22 15:33:50 +0900

    Updated seeds to only generate user names and passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,50 +7,106 @@ Project.destroy_all
 User.destroy_all
 
 puts "Creating users..."
-users = []
-5.times do
-  users << User.create!(
-    email: Faker::Internet.unique.email,
-    password: "password123",
-    password_confirmation: "password123"
-  )
-end
+User.create!(email: "andrew@projectsenpai.com", password: "123456", password_confirmation: "123456" )
+User.create!(email: "yuichi@projectsenpai.com", password: "123456", password_confirmation: "123456" )
+User.create!(email: "alonzo@projectsenpai.com", password: "123456", password_confirmation: "123456" )
+User.create!(email: "richard@projectsenpai.com", password: "123456", password_confirmation: "123456" )
 
-puts "Creating projects..."
-projects = []
-users.each do |user|
-  rand(2..4).times do
-    projects << Project.create!(
-      name: Faker::App.name,
-      description: Faker::Lorem.paragraph(sentence_count: 4),
-      user: user
-    )
-  end
-end
 
-puts "Creating features..."
-features = []
-projects.each do |project|
-  rand(3..6).times do
-    features << Feature.create!(
-      name: "#{Faker::Hacker.verb.capitalize} #{Faker::Hacker.noun}",
-      date_finished: Faker::Date.between(from: 1.year.ago, to: Date.today),
-      summary: Faker::Lorem.paragraph(sentence_count: 3),
-      project: project
-    )
-  end
-end
+# 5.times do
+#   users << User.create!(
+#     email: Faker::Internet.unique.email,
+#     password: "password123",
+#     password_confirmation: "password123"
+#   )
+# end
 
-puts "Creating messages..."
-features.each do |feature|
-  rand(2..5).times do
-    Message.create!(
-      role: ["user", "system", "assistant"].sample,
-      content: Faker::Lorem.sentence(word_count: 10),
-      feature: feature
-    )
-  end
-end
+# puts "Creating projects..."
+
+# Project.create!(name: "Regex generator",
+# description: "I want to make an app that will help to construct regex commands. I want to make a really useful UI that's easy for beginners",
+# user: user)
+
+# Project.create!(name: "Solar panel power generation UI",
+# description: "I want to make a website that will track the amount of power my solar panels are generating",
+# user: user)
+
+# Project.create!(name: "Focus app",
+#       description: "I want to make an app that will keep me from using my phone so much.",
+#       user: user)
+
+#       Project.create!(name: "Regex generator",
+# description: "I want to make an app that will help to construct regex commands. I want to make a really useful UI that's easy for beginners",
+# user: user)
+
+# Project.create!(name: "Solar panel power generation UI",
+# description: "I want to make a website that will track the amount of power my solar panels are generating",
+# user: user)
+
+# Project.create!(name: "Focus app",
+#       description: "I want to make an app that will keep me from using my phone so much.",
+#       user: user)
+
+#         Project.create!(name: "Regex generator",
+# description: "I want to make an app that will help to construct regex commands. I want to make a really useful UI that's easy for beginners",
+# user: user)
+
+# Project.create!(name: "Solar panel power generation UI",
+# description: "I want to make a website that will track the amount of power my solar panels are generating",
+# user: user)
+
+# Project.create!(name: "Focus app",
+#       description: "I want to make an app that will keep me from using my phone so much.",
+#       user: user)
+
+#         Project.create!(name: "Regex generator",
+# description: "I want to make an app that will help to construct regex commands. I want to make a really useful UI that's easy for beginners",
+# user: user)
+
+# Project.create!(name: "Solar panel power generation UI",
+# description: "I want to make a website that will track the amount of power my solar panels are generating",
+# user: user)
+
+# Project.create!(name: "Focus app",
+#       description: "I want to make an app that will keep me from using my phone so much.",
+#       user: user)
+
+
+
+# projects = []
+# users.each do |user|
+#   rand(2..4).times do
+#     projects << Project.create!(
+#       name: Faker::App.name,
+#       description: Faker::Lorem.paragraph(sentence_count: 4),
+#       user: user
+#     )
+#   end
+# end
+
+# puts "Creating features..."
+# features = []
+# projects.each do |project|
+#   rand(3..6).times do
+#     features << Feature.create!(
+#       name: "#{Faker::Hacker.verb.capitalize} #{Faker::Hacker.noun}",
+#       date_finished: Faker::Date.between(from: 1.year.ago, to: Date.today),
+#       summary: Faker::Lorem.paragraph(sentence_count: 3),
+#       project: project
+#     )
+#   end
+# end
+
+# puts "Creating messages..."
+# features.each do |feature|
+#   rand(2..5).times do
+#     Message.create!(
+#       role: ["user", "system", "assistant"].sample,
+#       content: Faker::Lorem.sentence(word_count: 10),
+#       feature: feature
+#     )
+#   end
+# end
 
 puts "✅ Done! Created:"
 puts "  #{User.count} users"
